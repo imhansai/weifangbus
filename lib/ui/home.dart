e7e2ea73b757924d0746698ced7bf2cc88ac8fca
韩塞 <imhansai@foxmail.com>
2019-01-17 14:45:40 +0800

    不断的尝试.....

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -15,40 +15,40 @@ class _HomePageState extends State<HomePage> {
       appBar: new AppBar(
         title: new Text('首页'),
       ),
-      body: new Column(
-        children: <Widget>[
-          /// 轮播图
-          new Container(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            width: MediaQuery.of(context).size.width,
-            height: 200.0,
-            child: new Swiper(
-              itemBuilder: (BuildContext context, int index) {
-                return _buildItem(context, index);
-              },
-              index: 0,
-              autoplay: true,
-              itemCount: 4,
-              viewportFraction: 0.8,
-              scale: 0.9,
-              duration: 300,
-              autoplayDelay: 5000,
-              pagination: new SwiperPagination(),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            /// 轮播图
+            new Container(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+              width: MediaQuery.of(context).size.width,
+              height: 200.0,
+              child: new Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return ClipRRect(
+                    borderRadius: new BorderRadius.all(new Radius.circular(_radius)),
+                    child: new FadeInImage.assetNetwork(
+                      placeholder: 'assets/home/placeholder.png',
+                      image:
+                          "http://122.4.254.30:3009//erp//InfoIsland//App//banner%E7%AE%A1%E7%90%86_170724112250_%E5%85%B3%E6%B3%A8%E5%B8%82%E6%B0%91%E5%87%BA%E8%A1%8C.jpg",
+                      fadeInCurve: Curves.easeIn,
+                      fadeInDuration: Duration(seconds: 1),
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                },
+                index: 0,
+                autoplay: true,
+                itemCount: 4,
+                viewportFraction: 0.8,
+                scale: 0.9,
+                duration: 300,
+                autoplayDelay: 3000,
+                pagination: new SwiperPagination(),
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  /// 为轮播图片加圆角
-  Widget _buildItem(BuildContext context, int index) {
-    /// ClipRRect: https://docs.flutter.io/flutter/widgets/ClipRRect-class.html
-    return ClipRRect(
-      borderRadius: new BorderRadius.all(new Radius.circular(_radius)),
-      child: new Image.network(
-        "http://via.placeholder.com/288x188",
-        fit: BoxFit.fill,
+          ],
+        ),
       ),
     );
   }
