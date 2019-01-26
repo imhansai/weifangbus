8e81dd5acd35a2b63fdcac9b60f24738aab22571
韩塞 <imhansai@foxmail.com>
2019-01-26 11:07:57 +0800

    修复错误

--- lib/entity_factory.dart
+++ lib/entity_factory.dart
@@ -1,9 +1,12 @@
 import 'package:weifangbus/entity/home/news_entity.dart';
+import 'package:weifangbus/entity/home/startUpBasicInfo_entity.dart';
 
 class EntityFactory {
   static T generateOBJ<T>(json) {
     if (1 == 0) {
       return null;
+    } else if (T.toString() == "StartUpBasicInfoEntity") {
+      return StartUpBasicInfoEntity.fromJson(json) as T;
     } else if (T.toString() == "NewsEntity") {
       return NewsEntity.fromJson(json) as T;
     } else {
