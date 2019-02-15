a3e22bb7e9eb31734f14c20c8a67b0f75f0ab07d
韩塞 <imhansai@foxmail.com>
2019-02-15 09:41:10 +0800

    i18n

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:weifangbus/generated/application.dart';
+import 'package:weifangbus/generated/translations.dart';
 import 'package:weifangbus/ui/homePage.dart';
 
-class WeiFangBusApp extends StatelessWidget {
+class WeiFangBusApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _WeiFangBusApp();
+  }
+}
+
+class _WeiFangBusApp extends State<WeiFangBusApp> {
+  SpecificLocalizationDelegate _localeOverrideDelegate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// 初始化一个新的Localization Delegate，有了它，当用户选择一种新的工作语言时，可以强制初始化一个新的Translations
+    _localeOverrideDelegate = new SpecificLocalizationDelegate(null);
+
+    /// 保存这个方法的指针，当用户改变语言时，我们可以调用applic.onLocaleChanged(new Locale('en',''));，通过SetState()我们可以强制App整个刷新
+    applic.onLocaleChanged = onLocaleChange;
+  }
+
+  /// 改变语言时的应用刷新核心，每次选择一种新的语言时，都会创造一个新的SpecificLocalizationDelegate实例，强制Translations类刷新。
+  onLocaleChange(Locale locale) {
+    setState(() {
+      _localeOverrideDelegate = new SpecificLocalizationDelegate(locale);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '潍坊公交',
       home: HomePage(),
+      localizationsDelegates: [
+        _localeOverrideDelegate, // 注册一个新的delegate
+        const TranslationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: applic.supportedLocales(),
     );
   }
 }
