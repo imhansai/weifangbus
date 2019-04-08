ee077d3421cd1921239f169fce75406a4d906a48
韩塞 <imhansai@foxmail.com>
2019-04-08 21:33:31 +0800

    调整结构，准备实验其他功能

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
 import 'package:weifangbus/ui/home.dart';
 
 class WeiFangBusApp extends StatefulWidget {
@@ -9,6 +13,15 @@ class WeiFangBusApp extends StatefulWidget {
 }
 
 class _WeiFangBusApp extends State<WeiFangBusApp> {
+  String debugLable = 'Unknown';
+  final JPush jpush = new JPush();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,5 +31,66 @@ class _WeiFangBusApp extends State<WeiFangBusApp> {
       ),
       home: Home(),
     );
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // 获取 registrationId，这个 JPush 运行通过 registrationId 来进行推送.
+    jpush.getRegistrationID().then((rid) {
+      setState(() {
+        debugLable = "flutter getRegistrationID: $rid";
+      });
+    });
+
+    jpush.setup(
+      appKey: "f0ac3212b863478a3ee578d5",
+      channel: "theChannel",
+      production: false,
+      debug: true, // 设置是否打印 debug 日志
+    );
+    // 申请推送权限，注意这个方法只会向用户弹出一次推送权限请求（如果用户不同意，之后只能用户到设置页面里面勾选相应权限），需要开发者选择合适的时机调用。
+    // 注意： iOS10+ 可以通过该方法来设置推送是否前台展示，是否触发声音，是否设置应用角标 badge
+    jpush.applyPushAuthority(new NotificationSettingsIOS(sound: true, alert: true, badge: true));
+
+    try {
+      // 添加事件监听方法
+      jpush.addEventHandler(
+        // 接收通知回调方法
+        onReceiveNotification: (Map<String, dynamic> message) async {
+          print("flutter onReceiveNotification: $message");
+          setState(() {
+            debugLable = "flutter onReceiveNotification: $message";
+          });
+        },
+        // 点击通知回调方法
+        onOpenNotification: (Map<String, dynamic> message) async {
+          print("flutter onOpenNotification: $message");
+          setState(() {
+            debugLable = "flutter onOpenNotification: $message";
+          });
+        },
+        // 接收自定义消息回调方法
+        onReceiveMessage: (Map<String, dynamic> message) async {
+          print("flutter onReceiveMessage: $message");
+          setState(() {
+            debugLable = "flutter onReceiveMessage: $message";
+          });
+        },
+      );
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      debugLable = platformVersion;
+    });
   }
 }
--- lib/ui/explore/explore_page.dart
+++ lib/ui/explore/explore_page.dart
@@ -1,252 +1,26 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:jpush_flutter/jpush_flutter.dart';
 
 class ExplorePage extends StatefulWidget {
   @override
-  _ExplorePageState createState() => new _ExplorePageState();
+  _ExplorePageState createState() => _ExplorePageState();
 }
 
-class _ExplorePageState extends State<ExplorePage> {
-  String debugLable = 'Unknown';
-  final JPush jpush = new JPush();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    jpush.getRegistrationID().then((rid) {
-      setState(() {
-        debugLable = "flutter getRegistrationID: $rid";
-      });
-    });
-
-    jpush.setup(
-      appKey: "f0ac3212b863478a3ee578d5",
-      channel: "theChannel",
-      production: false,
-      debug: true,
-    );
-    jpush.applyPushAuthority(new NotificationSettingsIOS(sound: true, alert: true, badge: true));
-
-    try {
-      jpush.addEventHandler(
-        onReceiveNotification: (Map<String, dynamic> message) async {
-          print("flutter onReceiveNotification: $message");
-          setState(() {
-            debugLable = "flutter onReceiveNotification: $message";
-          });
-        },
-        onOpenNotification: (Map<String, dynamic> message) async {
-          print("flutter onOpenNotification: $message");
-          setState(() {
-            debugLable = "flutter onOpenNotification: $message";
-          });
-        },
-        onReceiveMessage: (Map<String, dynamic> message) async {
-          print("flutter onReceiveMessage: $message");
-          setState(() {
-            debugLable = "flutter onReceiveMessage: $message";
-          });
-        },
-      );
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      debugLable = platformVersion;
-    });
-  }
-
-  // 编写视图
+class _ExplorePageState extends State<ExplorePage> with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: new Center(
-            child: new Column(children: [
-          new Text('result: $debugLable\n'),
-          new FlatButton(
-              child: new Text('sendLocalNotification\n'),
-              onPressed: () {
-                // 三秒后出发本地推送
-                var fireDate = DateTime.fromMillisecondsSinceEpoch(DateTime.now().millisecondsSinceEpoch + 3000);
-                var localNotification = LocalNotification(
-                    id: 234,
-                    title: 'fadsfa',
-                    buildId: 1,
-                    content: 'fdas',
-                    fireTime: fireDate,
-                    subtitle: 'fasf',
-                    badge: 5,
-                    extras: {"fa": "0"});
-                jpush.sendLocalNotification(localNotification).then((res) {
-                  setState(() {
-                    debugLable = res;
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('getLaunchAppNotification\n'),
-              onPressed: () {
-                jpush.getLaunchAppNotification().then((map) {
-                  setState(() {
-                    debugLable = "getLaunchAppNotification success: $map";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "getLaunchAppNotification error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('applyPushAuthority\n'),
-              onPressed: () {
-                jpush.applyPushAuthority(NotificationSettingsIOS(badge: true, alert: true, sound: true));
-              }),
-          new FlatButton(
-              child: new Text('setTags\n'),
-              onPressed: () {
-                jpush.setTags(["lala", "haha"]).then((map) {
-                  var tags = map['tags'];
-                  setState(() {
-                    debugLable = "set tags success: $map $tags";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "set tags error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('cleanTags\n'),
-              onPressed: () {
-                jpush.cleanTags().then((map) {
-                  var tags = map['tags'];
-                  setState(() {
-                    debugLable = "cleanTags success: $map $tags";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "cleanTags error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('addTags\n'),
-              onPressed: () {
-                jpush.addTags(["lala", "haha"]).then((map) {
-                  var tags = map['tags'];
-                  setState(() {
-                    debugLable = "addTags success: $map $tags";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "addTags error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('deleteTags\n'),
-              onPressed: () {
-                jpush.deleteTags(["lala", "haha"]).then((map) {
-                  var tags = map['tags'];
-                  setState(() {
-                    debugLable = "deleteTags success: $map $tags";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "deleteTags error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('getAllTags\n'),
-              onPressed: () {
-                jpush.getAllTags().then((map) {
-                  setState(() {
-                    debugLable = "getAllTags success: $map";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "getAllTags error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('setAlias\n'),
-              onPressed: () {
-                jpush.setAlias("thealias11").then((map) {
-                  setState(() {
-                    debugLable = "setAlias success: $map";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "setAlias error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('deleteAlias\n'),
-              onPressed: () {
-                jpush.deleteAlias().then((map) {
-                  setState(() {
-                    debugLable = "deleteAlias success: $map";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "deleteAlias error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('setBadge\n'),
-              onPressed: () {
-                jpush.setBadge(66).then((map) {
-                  setState(() {
-                    debugLable = "setBadge success: $map";
-                  });
-                }).catchError((error) {
-                  setState(() {
-                    debugLable = "setBadge error: $error";
-                  });
-                });
-              }),
-          new FlatButton(
-              child: new Text('stopPush\n'),
-              onPressed: () {
-                jpush.stopPush();
-              }),
-          new FlatButton(
-              child: new Text('resumePush\n'),
-              onPressed: () {
-                jpush.resumePush();
-              }),
-          new FlatButton(
-              child: new Text('clearAllNotifications\n'),
-              onPressed: () {
-                jpush.clearAllNotifications();
-              }),
-        ])),
+    super.build(context);
+    return Scaffold(
+      key: PageStorageKey(_ExplorePageState),
+      appBar: AppBar(
+        title: Text("发现"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("探索"),
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
