664fdce7a740f2241fdee636741a03d30bcfa6e7
韩塞 <imhansai@foxmail.com>
2019-05-11 07:46:06 +0800

    强制竖屏

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:weifangbus/ui/app.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(WeiFangBusApp());
+void main() {
+  // 强制竖屏
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(WeiFangBusApp());
+  });
+}
