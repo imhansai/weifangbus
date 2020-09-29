6cdda73aa0aa4d0c78e8905d347c72728c2ea294
hanandjun <imhansai@foxmail.com>
2020-09-29 17:08:35 +0800

    按比例弹性布局

--- lib/view/home/line/route_detail.dart
+++ lib/view/home/line/route_detail.dart
@@ -165,195 +165,205 @@ class _RouteDetailState extends State<RouteDetail>
           var length = _routeStatData.segmentlist.length;
           var widgets = <Widget>[
             // 线路名称
-            Padding(
-              padding: EdgeInsets.all(
-                ScreenUtil().setWidth(20),
-              ),
-              child: Flex(
-                direction: Axis.horizontal,
-                children: [
-                  // 线路名称
-                  Text(
-                    _routeStatData.routename,
-                    style: TextStyle(
-                      fontSize: ScreenUtil().setSp(70),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: EdgeInsets.all(
+                  ScreenUtil().setWidth(20),
+                ),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    // 线路名称
+                    Text(
+                      _routeStatData.routename,
+                      style: TextStyle(
+                        fontSize: ScreenUtil().setSp(70),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             // xxx -> xxx
-            Padding(
-              padding: EdgeInsets.only(
-                left: ScreenUtil().setWidth(20),
-                bottom: ScreenUtil().setHeight(20),
-                right: ScreenUtil().setWidth(20),
-              ),
-              child: Flex(
-                direction: Axis.horizontal,
-                children: [
-                  // 起点站
-                  Expanded(
-                    // flex: 3,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Colors.orange, Colors.orange[700]],
+            Expanded(
+              flex: 4,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: ScreenUtil().setWidth(20),
+                  bottom: ScreenUtil().setHeight(20),
+                  right: ScreenUtil().setWidth(20),
+                ),
+                child: Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    // 起点站
+                    Expanded(
+                      // flex: 3,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Colors.orange, Colors.orange[700]],
+                          ),
+                          borderRadius: BorderRadius.circular(3.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 4.0,
+                            )
+                          ],
                         ),
-                        borderRadius: BorderRadius.circular(3.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black54,
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 4.0,
-                          )
+                        child: Padding(
+                          padding: EdgeInsets.all(
+                            ScreenUtil().setWidth(20),
+                          ),
+                          child: AutoSizeText(
+                            _segment.stationlist.first.stationname,
+                            style: TextStyle(
+                              fontSize: ScreenUtil().setSp(50),
+                            ),
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // 方向 icon + 换向
+                    Expanded(
+                      // flex: 1,
+                      child: Flex(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        direction: Axis.vertical,
+                        children: [
+                          Expanded(
+                            child: Icon(
+                              Icons.forward,
+                              color: Colors.orange,
+                            ),
+                          ),
+                          // 换向
+                          length > 1
+                              ? Expanded(
+                                  child: RaisedButton.icon(
+                                    color: Colors.green,
+                                    icon: Icon(
+                                      Icons.swap_horiz,
+                                      size: 15,
+                                    ),
+                                    label: Text(
+                                      '换向',
+                                      style: TextStyle(fontSize: 13),
+                                    ),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _index == 0 ? _index = 1 : _index = 0;
+                                        _segment =
+                                            _routeStatData.segmentlist[_index];
+                                        print('段 id ${_segment.segmentid}');
+                                        _segmentID =
+                                            _segment.segmentid.toString();
+                                        print(_segmentID);
+                                        print('重新设置定时器');
+                                        _timer?.cancel();
+                                        _immediatelyFlush(_segmentID);
+                                        _refreshRouteRealTimeInfo(_segmentID);
+                                      });
+                                    },
+                                  ),
+                                )
+                              : Container()
                         ],
                       ),
-                      child: Padding(
-                        padding: EdgeInsets.all(
-                          ScreenUtil().setWidth(20),
-                        ),
-                        child: AutoSizeText(
-                          _segment.stationlist.first.stationname,
-                          style: TextStyle(
-                            fontSize: ScreenUtil().setSp(50),
+                    ),
+                    // 终点站
+                    Expanded(
+                      // flex: 3,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Colors.orange, Colors.orange[700]],
                           ),
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
+                          borderRadius: BorderRadius.circular(3.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black54,
+                              offset: Offset(2.0, 2.0),
+                              blurRadius: 4.0,
+                            )
+                          ],
                         ),
-                      ),
-                    ),
-                  ),
-                  // 方向 icon + 换向
-                  Expanded(
-                    // flex: 1,
-                    child: Flex(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      direction: Axis.vertical,
-                      children: [
-                        Icon(
-                          Icons.forward,
-                          color: Colors.orange,
-                        ),
-                        // 换向
-                        length > 1
-                            ? SizedBox(
-                                height: ScreenUtil().setHeight(80),
-                                child: RaisedButton.icon(
-                                  color: Colors.green,
-                                  icon: Icon(
-                                    Icons.swap_horiz,
-                                    size: 15,
-                                  ),
-                                  label: Text(
-                                    '换向',
-                                    style: TextStyle(fontSize: 13),
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      _index == 0 ? _index = 1 : _index = 0;
-                                      _segment =
-                                          _routeStatData.segmentlist[_index];
-                                      print('段 id ${_segment.segmentid}');
-                                      _segmentID =
-                                          _segment.segmentid.toString();
-                                      print(_segmentID);
-                                      print('重新设置定时器');
-                                      _timer?.cancel();
-                                      _immediatelyFlush(_segmentID);
-                                      _refreshRouteRealTimeInfo(_segmentID);
-                                    });
-                                  },
-                                ),
-                              )
-                            : Container()
-                      ],
-                    ),
-                  ),
-                  // 终点站
-                  Expanded(
-                    // flex: 3,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [Colors.orange, Colors.orange[700]],
-                        ),
-                        borderRadius: BorderRadius.circular(3.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black54,
-                            offset: Offset(2.0, 2.0),
-                            blurRadius: 4.0,
-                          )
-                        ],
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(
-                          ScreenUtil().setWidth(20),
-                        ),
-                        child: AutoSizeText(
-                          _segment.stationlist.last.stationname,
-                          style: TextStyle(
-                            fontSize: ScreenUtil().setSp(50),
+                        child: Padding(
+                          padding: EdgeInsets.all(
+                            ScreenUtil().setWidth(20),
                           ),
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
+                          child: AutoSizeText(
+                            _segment.stationlist.last.stationname,
+                            style: TextStyle(
+                              fontSize: ScreenUtil().setSp(50),
+                            ),
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             // 首末班 + 票价
-            Padding(
-              padding: EdgeInsets.only(
-                left: ScreenUtil().setWidth(20),
-                // top: ScreenUtil().setHeight(20),
-                right: ScreenUtil().setHeight(20),
-                bottom: ScreenUtil().setHeight(20),
-              ),
-              child: Flex(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                direction: Axis.horizontal,
-                children: [
-                  // 首末班
-                  Expanded(
-                    child: AutoSizeText(
-                      _segment.firtlastshiftinfo,
-                      style: TextStyle(
-                        fontSize: ScreenUtil().setSp(45),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: ScreenUtil().setWidth(20),
+                  // top: ScreenUtil().setHeight(20),
+                  right: ScreenUtil().setHeight(20),
+                  bottom: ScreenUtil().setHeight(20),
+                ),
+                child: Flex(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  direction: Axis.horizontal,
+                  children: [
+                    // 首末班
+                    Expanded(
+                      child: AutoSizeText(
+                        _segment.firtlastshiftinfo,
+                        style: TextStyle(
+                          fontSize: ScreenUtil().setSp(45),
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
+                      flex: 4,
                     ),
-                    flex: 4,
-                  ),
-                  // 空白填充
-                  Expanded(
-                    child: Container(),
-                    flex: 1,
-                  ),
-                  // 票价
-                  Expanded(
-                    child: AutoSizeText(
-                      _segment.routeprice.contains('票价')
-                          ? _segment.routeprice
-                          : '票价: ${_segment.routeprice} 元',
-                      style: TextStyle(
-                        fontSize: ScreenUtil().setSp(45),
+                    // 空白填充
+                    Expanded(
+                      child: Container(),
+                      flex: 1,
+                    ),
+                    // 票价
+                    Expanded(
+                      child: AutoSizeText(
+                        _segment.routeprice.contains('票价')
+                            ? _segment.routeprice
+                            : '票价: ${_segment.routeprice} 元',
+                        style: TextStyle(
+                          fontSize: ScreenUtil().setSp(45),
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                    flex: 4,
-                  )
-                ],
+                      flex: 4,
+                    )
+                  ],
+                ),
               ),
             )
           ];
@@ -361,29 +371,13 @@ class _RouteDetailState extends State<RouteDetail>
           return Column(
             children: [
               // 头部信息
-              Padding(
-                padding: EdgeInsets.all(
-                  ScreenUtil().setWidth(20),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.blueGrey, Colors.blueGrey[400]],
-                    ),
-                    borderRadius: BorderRadius.circular(12.0),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black54,
-                          offset: Offset(2.0, 2.0),
-                          blurRadius: 4.0),
-                    ],
-                  ),
-                  // color: Colors.blueGrey,
-                  height: ScreenUtil().setHeight(430),
-                  width: double.infinity,
-                  child: ListView(
-                    children: widgets,
-                  ),
+              Container(
+                color: Colors.blueGrey,
+                height: ScreenUtil().setHeight(400),
+                width: double.infinity,
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: widgets,
                 ),
               ),
               // 站点列表
