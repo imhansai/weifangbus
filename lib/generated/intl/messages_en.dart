e66833fc86be4daca8856d3b76d35e285e9265d9
iasnah <imhansai@foxmail.com>
2021-08-11 09:40:55 +0800

    支持空安全

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,70 +19,89 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  static m0(num) => "${num} arrive";
+  static String m0(num) => "${num} arrive";
 
-  static m1(num) => "${num} leave";
+  static String m1(num) => "${num} leave";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "AboutSoftWare" : MessageLookupByLibrary.simpleMessage("About weifang Bus"),
-    "Age" : MessageLookupByLibrary.simpleMessage("Age"),
-    "AgeDesc" : MessageLookupByLibrary.simpleMessage("25"),
-    "AppDesc" : MessageLookupByLibrary.simpleMessage("This App was made by the technique of Flutter after personal work. The purpose is to run a bit more modern, I hope you like it😜。"),
-    "AppName" : MessageLookupByLibrary.simpleMessage("WeiFang Bus"),
-    "AppearanceManagement" : MessageLookupByLibrary.simpleMessage("Appearance Management"),
-    "ArriveAtStation" : m0,
-    "Author" : MessageLookupByLibrary.simpleMessage("Come to the author"),
-    "Auto" : MessageLookupByLibrary.simpleMessage("Follow the system"),
-    "AwayFromTheStation" : m1,
-    "BackToTop" : MessageLookupByLibrary.simpleMessage("Back to top"),
-    "Chinese" : MessageLookupByLibrary.simpleMessage("Chinese"),
-    "City" : MessageLookupByLibrary.simpleMessage("City"),
-    "CityDesc" : MessageLookupByLibrary.simpleMessage("ShanDong WeiFang"),
-    "Company" : MessageLookupByLibrary.simpleMessage("Weifang Public Transportation Corporation"),
-    "CompanyDesc" : MessageLookupByLibrary.simpleMessage("About Weifang Public Transportation Corporation"),
-    "Dark" : MessageLookupByLibrary.simpleMessage("Dark"),
-    "EMail" : MessageLookupByLibrary.simpleMessage("E-mail"),
-    "English" : MessageLookupByLibrary.simpleMessage("English"),
-    "ExitApp" : MessageLookupByLibrary.simpleMessage("click again to exit the application"),
-    "Explore" : MessageLookupByLibrary.simpleMessage("Explore"),
-    "Guide" : MessageLookupByLibrary.simpleMessage("Guide"),
-    "Home" : MessageLookupByLibrary.simpleMessage("Home"),
-    "HomeNews" : MessageLookupByLibrary.simpleMessage("News"),
-    "Japanese" : MessageLookupByLibrary.simpleMessage("Japanese"),
-    "LanguageSettings" : MessageLookupByLibrary.simpleMessage("Language Settings"),
-    "Light" : MessageLookupByLibrary.simpleMessage("Light"),
-    "Loading" : MessageLookupByLibrary.simpleMessage("Loading..."),
-    "More" : MessageLookupByLibrary.simpleMessage("More"),
-    "Name" : MessageLookupByLibrary.simpleMessage("Name"),
-    "NameDesc" : MessageLookupByLibrary.simpleMessage("Han Sai"),
-    "News" : MessageLookupByLibrary.simpleMessage("News"),
-    "NewsDate" : MessageLookupByLibrary.simpleMessage("yyyy-MM-dd"),
-    "NewsDetail" : MessageLookupByLibrary.simpleMessage("News Detail"),
-    "NewsDetailDate" : MessageLookupByLibrary.simpleMessage("yyyy-MM-dd HH:mm:ss"),
-    "NoNews" : MessageLookupByLibrary.simpleMessage("No News"),
-    "NoPictures" : MessageLookupByLibrary.simpleMessage("No Pictures"),
-    "NonWidgetLabel" : MessageLookupByLibrary.simpleMessage("Not currently connected to any asynchronous computation."),
-    "NotConnectedToAnyNetwork" : MessageLookupByLibrary.simpleMessage("The device is not connected to any network. Please try again after connecting to the network!"),
-    "NotInstalledQQ" : MessageLookupByLibrary.simpleMessage("Sorry, I have detected that you have not installed the QQ client"),
-    "Ok" : MessageLookupByLibrary.simpleMessage("Ok"),
-    "Phone" : MessageLookupByLibrary.simpleMessage("Phone"),
-    "QQGroup" : MessageLookupByLibrary.simpleMessage("QQ Group"),
-    "QQGroupDesc" : MessageLookupByLibrary.simpleMessage("Join QQ group to exchange feedback"),
-    "RefreshSuccess" : MessageLookupByLibrary.simpleMessage("Refresh the success!"),
-    "ReleaseTime" : MessageLookupByLibrary.simpleMessage("Release Time: "),
-    "Repo" : MessageLookupByLibrary.simpleMessage("code repository"),
-    "RequestDataFailure" : MessageLookupByLibrary.simpleMessage("Request data failed. Try switching network and retry!"),
-    "Reversing" : MessageLookupByLibrary.simpleMessage("Reversing"),
-    "ReversingComplete" : MessageLookupByLibrary.simpleMessage("Reversing Complete"),
-    "RouteQuery" : MessageLookupByLibrary.simpleMessage("Route Query"),
-    "SearchHistory" : MessageLookupByLibrary.simpleMessage("This is a search record that is about to be perfected"),
-    "SearchLine" : MessageLookupByLibrary.simpleMessage("Search Line"),
-    "SeeSource" : MessageLookupByLibrary.simpleMessage("To view the source code for this application, visit: "),
-    "Selected" : MessageLookupByLibrary.simpleMessage("Selected"),
-    "Settings" : MessageLookupByLibrary.simpleMessage("Settings"),
-    "SettingsDesc" : MessageLookupByLibrary.simpleMessage("Perfect regulation is under your control"),
-    "SoftWare" : MessageLookupByLibrary.simpleMessage("Weifang Bus"),
-    "SpecialInstructions" : MessageLookupByLibrary.simpleMessage("Special note: The interface is provided by [Weifang Zhangshangbus]. If there is any infringement, please contact me。")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "AboutSoftWare":
+            MessageLookupByLibrary.simpleMessage("About weifang Bus"),
+        "Age": MessageLookupByLibrary.simpleMessage("Age"),
+        "AgeDesc": MessageLookupByLibrary.simpleMessage("25"),
+        "AppDesc": MessageLookupByLibrary.simpleMessage(
+            "This App was made by the technique of Flutter after personal work. The purpose is to run a bit more modern, I hope you like it😜。"),
+        "AppName": MessageLookupByLibrary.simpleMessage("WeiFang Bus"),
+        "AppearanceManagement":
+            MessageLookupByLibrary.simpleMessage("Appearance Management"),
+        "ArriveAtStation": m0,
+        "Author": MessageLookupByLibrary.simpleMessage("Come to the author"),
+        "Auto": MessageLookupByLibrary.simpleMessage("Follow the system"),
+        "AwayFromTheStation": m1,
+        "BackToTop": MessageLookupByLibrary.simpleMessage("Back to top"),
+        "Chinese": MessageLookupByLibrary.simpleMessage("Chinese"),
+        "City": MessageLookupByLibrary.simpleMessage("City"),
+        "CityDesc": MessageLookupByLibrary.simpleMessage("ShanDong WeiFang"),
+        "Company": MessageLookupByLibrary.simpleMessage(
+            "Weifang Public Transportation Corporation"),
+        "CompanyDesc": MessageLookupByLibrary.simpleMessage(
+            "About Weifang Public Transportation Corporation"),
+        "Dark": MessageLookupByLibrary.simpleMessage("Dark"),
+        "EMail": MessageLookupByLibrary.simpleMessage("E-mail"),
+        "English": MessageLookupByLibrary.simpleMessage("English"),
+        "ExitApp": MessageLookupByLibrary.simpleMessage(
+            "click again to exit the application"),
+        "Explore": MessageLookupByLibrary.simpleMessage("Explore"),
+        "Guide": MessageLookupByLibrary.simpleMessage("Guide"),
+        "Home": MessageLookupByLibrary.simpleMessage("Home"),
+        "HomeNews": MessageLookupByLibrary.simpleMessage("News"),
+        "Japanese": MessageLookupByLibrary.simpleMessage("Japanese"),
+        "LanguageSettings":
+            MessageLookupByLibrary.simpleMessage("Language Settings"),
+        "Light": MessageLookupByLibrary.simpleMessage("Light"),
+        "Loading": MessageLookupByLibrary.simpleMessage("Loading..."),
+        "More": MessageLookupByLibrary.simpleMessage("More"),
+        "Name": MessageLookupByLibrary.simpleMessage("Name"),
+        "NameDesc": MessageLookupByLibrary.simpleMessage("Han Sai"),
+        "News": MessageLookupByLibrary.simpleMessage("News"),
+        "NewsDate": MessageLookupByLibrary.simpleMessage("yyyy-MM-dd"),
+        "NewsDetail": MessageLookupByLibrary.simpleMessage("News Detail"),
+        "NewsDetailDate":
+            MessageLookupByLibrary.simpleMessage("yyyy-MM-dd HH:mm:ss"),
+        "NoNews": MessageLookupByLibrary.simpleMessage("No News"),
+        "NoPictures": MessageLookupByLibrary.simpleMessage("No Pictures"),
+        "NonWidgetLabel": MessageLookupByLibrary.simpleMessage(
+            "Not currently connected to any asynchronous computation."),
+        "NotConnectedToAnyNetwork": MessageLookupByLibrary.simpleMessage(
+            "The device is not connected to any network. Please try again after connecting to the network!"),
+        "NotInstalledQQ": MessageLookupByLibrary.simpleMessage(
+            "Sorry, I have detected that you have not installed the QQ client"),
+        "Ok": MessageLookupByLibrary.simpleMessage("Ok"),
+        "Phone": MessageLookupByLibrary.simpleMessage("Phone"),
+        "QQGroup": MessageLookupByLibrary.simpleMessage("QQ Group"),
+        "QQGroupDesc": MessageLookupByLibrary.simpleMessage(
+            "Join QQ group to exchange feedback"),
+        "RefreshSuccess":
+            MessageLookupByLibrary.simpleMessage("Refresh the success!"),
+        "ReleaseTime": MessageLookupByLibrary.simpleMessage("Release Time: "),
+        "Repo": MessageLookupByLibrary.simpleMessage("code repository"),
+        "RequestDataFailure": MessageLookupByLibrary.simpleMessage(
+            "Request data failed. Try switching network and retry!"),
+        "Reversing": MessageLookupByLibrary.simpleMessage("Reversing"),
+        "ReversingComplete":
+            MessageLookupByLibrary.simpleMessage("Reversing Complete"),
+        "RouteQuery": MessageLookupByLibrary.simpleMessage("Route Query"),
+        "SearchHistory": MessageLookupByLibrary.simpleMessage(
+            "This is a search record that is about to be perfected"),
+        "SearchLine": MessageLookupByLibrary.simpleMessage("Search Line"),
+        "SeeSource": MessageLookupByLibrary.simpleMessage(
+            "To view the source code for this application, visit: "),
+        "Selected": MessageLookupByLibrary.simpleMessage("Selected"),
+        "Settings": MessageLookupByLibrary.simpleMessage("Settings"),
+        "SettingsDesc": MessageLookupByLibrary.simpleMessage(
+            "Perfect regulation is under your control"),
+        "SoftWare": MessageLookupByLibrary.simpleMessage("Weifang Bus"),
+        "SpecialInstructions": MessageLookupByLibrary.simpleMessage(
+            "Special note: The interface is provided by [Weifang Zhangshangbus]. If there is any infringement, please contact me。")
+      };
 }
