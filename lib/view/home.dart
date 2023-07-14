ec489c670c3e93b31c07f6d5621434a40f7b5cee
imhansai <imhansai@foxmail.com>
2023-07-14 16:10:40 +0800

    去掉 WillPopScope

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -34,73 +34,41 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: Scaffold(
-        key: _homeKey,
-        body: PageView(
-          controller: _controller,
-          children: myTabs.map((Widget widget) {
-            return widget;
-          }).toList(),
-          physics: NeverScrollableScrollPhysics(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          type: BottomNavigationBarType.fixed,
-          onTap: (index) {
-            _controller.jumpToPage(index);
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-              ),
-              label: AppLocalizations.of(context)!.home,
+    return Scaffold(
+      key: _homeKey,
+      body: PageView(
+        controller: _controller,
+        children: myTabs.map((Widget widget) {
+          return widget;
+        }).toList(),
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          _controller.jumpToPage(index);
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.explore),
-              label: AppLocalizations.of(context)!.explore,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.more_vert),
-              label: AppLocalizations.of(context)!.more,
-            ),
-          ],
-        ),
+            label: AppLocalizations.of(context)!.home,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: AppLocalizations.of(context)!.explore,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_vert),
+            label: AppLocalizations.of(context)!.more,
+          ),
+        ],
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  /// 最近一次点击时间
-  late DateTime? _lastTime;
-
-  /// 再点一次退出程序
-  Future<bool> _onWillPop() {
-    if (_lastTime == null ||
-        DateTime.now().difference(_lastTime!) > Duration(milliseconds: 1500)) {
-      print('准备退出');
-      _lastTime = DateTime.now();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(
-            AppLocalizations.of(context)!.exitApp,
-            textAlign: TextAlign.center,
-          ),
-          duration: Duration(milliseconds: 1500),
-        ),
-      );
-      return Future.value(false);
-    }
-    print('退出了鸭,ByeBye');
-    return Future.value(true);
   }
 }
